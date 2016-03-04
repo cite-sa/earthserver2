79456b682f78cfa44622020b869b808af2fa128c
kapostolopoulos <pa72_k4>
2016-03-04 20:40:46 +0200

    usage comments added

--- service.js
+++ service.js
@@ -3,7 +3,10 @@ $(function () {
         options: {},
         _create: function () {
             var self = this;
+
+            /* main dock (required) */
             var leftDock = $("<div>").mainDock({container: this.element}).mainDock("instance");
+            /* */
             leftDock
                 .addProjectionSelectPanel(function(elemClicked) {
                     console.log(elemClicked.text())
@@ -11,17 +14,23 @@ $(function () {
                 .addAvailableCoveragesPanel()
                 .addQueryTerminalPanel();
 
-            var rightDock2 = $("<div>").dock({container: this.element, position: "right"}).dock("instance");
-            rightDock2.addEmptyPanel();
+            var exampleRightDock = $("<div>").dock({container: this.element, position: "right"}).dock("instance");
+            exampleRightDock.addEmptyPanel();
+
+            /* info dock (required) */
             var infoDock = $("<div>").infoDock({container: this.element}).infoDock("instance");
             var infoPanel = infoDock.addInfoPanel();
+
+            /* Example Content */
             infoPanel
                 .addTab("about", "about", "Title1", "SubTitle1", "jndfabv adfndfvbs dfasvikjadsfv")
                 .addTab("contact", "contact", "Title2", "SubTitle2", "afhofbon fbojirwegi0 fbh0bfeno")
                 .addTab("tour", "tour", "Title3", "SubTitle3", "dfabhijdfb fbohidfb dfboihbfd hoibdf ");
 
+            /* gis toolbar (required) */
             var gisToolbar = $("<div>").gisToolbar({container: this.element}).gisToolbar("instance");
             gisToolbar.addClickHandler("#zoom-in-tool", function() {console.log("Zoom in.")});
+            /* coordinates overlay (required) */
             var coordinates = $("<div>").coordinateOverlay({container: this.element}).coordinateOverlay("instance");
         }
     })
