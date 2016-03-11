938565cfe70056f720e95ee79869bba1e0cca9ec
kapostolopoulos <pa72_k4>
2016-03-11 17:32:39 +0200

    Changed icon

--- plot-dock.js
+++ plot-dock.js
@@ -2,7 +2,7 @@ $(function () {
     $.widget("dock.plotDock", $.earthserver.dock, {
         options: {
             position: "right",
-            toggleIcon: "images/icons/info(h100).png"
+            toggleIcon: "images/icons/diagram(h100).png"
         },
         _create: function () {
             this._super();
